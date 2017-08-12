d18269f9b8e5bd7ee9f69a0787ec0c7b99ff4703
JohnLiu <john831118@gmail.com>
2017-08-13 00:14:37 +0800

    Proposal Component and Create Proposal Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; 
 
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent,
     HomepageComponent,
-    DocumentsComponent
+    DocumentsComponent,
+    ProposalListComponent,
+    ProposalNewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/proposal/proposal-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Proposal } from './proposal'
+
+
+@Component({
+	moduleId: module.id,
+	selector: 'proposal-list',
+	templateUrl: 'proposal-list.component.html'
+})
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(0,'ABC Company','http://www.google.com','PHP',5,150,2,'john@gmail.com')
+	proposalTwo: Proposal = new Proposal(1,'DDD Company','http://www.google.com','PHP',5,150,2,'john@gmail.com')
+	proposalThree: Proposal = new Proposal(2,'RRR Company','http://www.google.com','PHP',5,150,2,'john@gmail.com')
+	
+	proposals: Proposal[] = [this.proposalOne,this.proposalTwo,this.proposalThree]
+}
