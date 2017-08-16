c35cd6b700c5578727a8b969540ee24f2622ee24
JohnLiu <john831118@gmail.com>
2017-08-17 00:51:18 +0800

    Bootstrap nav and card style

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
     		description: "Check",
     		file_url: "www.google.com",
     		updated_at: "2017-08-08",
-    		image_url: "www.yahoo.com"
+    		image_url: "https://2.bp.blogspot.com/--A6_TIBMAAk/V03HjisgTbI/AAAAAAAACDI/55o30gg0h8QWP67fUPHI_ASsoWeJBY4jQCLcB/s1600/freelancing.jpg"
 		},
 		{
 			title: "Test2",
     		description: "Check",
     		file_url: "www.google.com",
     		updated_at: "2017-08-08",
-    		image_url: "www.yahoo.com"
+    		image_url: "http://freelancer.rapidbusinesslessons.com/wp-content/uploads/2015/12/freelancer-sketch.png"
 		},
 		{
 			title: "Test3",
     		description: "Check",
     		file_url: "www.google.com",
     		updated_at: "2017-08-08",
-    		image_url: "www.yahoo.com"
+    		image_url: "http://freelancer.rapidbusinesslessons.com/wp-content/uploads/2015/12/freelancer-sketch.png"
 		}
 	]
 }
