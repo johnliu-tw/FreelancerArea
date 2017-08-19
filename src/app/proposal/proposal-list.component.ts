0672371739bd3d5cf0d03c31f97069082357fc0d
JohnLiu <john831118@gmail.com>
2017-08-19 11:13:15 +0800

    styling

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -5,12 +5,14 @@ import { Proposal } from './proposal'
 @Component({
 	moduleId: module.id,
 	selector: 'proposal-list',
-	templateUrl: 'proposal-list.component.html'
+	templateUrl: 'proposal-list.component.html',
+	styleUrls: ['proposal-list.component.css']
+
 })
 export class ProposalListComponent {
-	proposalOne: Proposal = new Proposal(0,'ABC Company','http://www.google.com','PHP',5,150,2,'john@gmail.com')
-	proposalTwo: Proposal = new Proposal(1,'DDD Company','http://www.google.com','PHP',5,150,2,'john@gmail.com')
-	proposalThree: Proposal = new Proposal(2,'RRR Company','http://www.google.com','PHP',5,150,2,'john@gmail.com')
+	proposalOne: Proposal = new Proposal(0,'ABC Company','http://www.google.com','PHP',5,1500,2,'john@gmail.com')
+	proposalTwo: Proposal = new Proposal(1,'DDD Company','http://www.google.com','PHP',5,1500,2,'john@gmail.com')
+	proposalThree: Proposal = new Proposal(2,'RRR Company','http://www.google.com','PHP',5,1500,2,'john@gmail.com')
 	
 	proposals: Proposal[] = [this.proposalOne,this.proposalTwo,this.proposalThree]
 }
